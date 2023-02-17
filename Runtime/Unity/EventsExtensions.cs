1e033dd04b18a0dd9ba72d9124806d4b7bc8004c
Mirzipan <mfarkas@pixelfederation.com>
2023-02-17 21:32:06 +0100

    ✨added disposable subscriptions to unity events

---
+++ Runtime/Unity/EventsExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Mirzipan.Bibliotheca.Disposables;
+using UnityEngine.Events;
+
+namespace Mirzipan.Bibliotheca.Unity
+{
+    public static class EventsExtensions
+    {
+        public static IDisposable Subscribe(
+            this UnityEvent @this, 
+            UnityAction action)
+        {
+            @this.AddListener(action);
+            return Disposable.Create(() => { @this.RemoveListener(action); });
+        }
+
+        public static IDisposable Subscribe<T0>(
+            this UnityEvent<T0> @this, 
+            UnityAction<T0> action)
+        {
+            @this.AddListener(action);
+            return Disposable.Create(() => { @this.RemoveListener(action); });
+        }
+
+        public static IDisposable Subscribe<T0, T1>(
+            this UnityEvent<T0, T1> @this, 
+            UnityAction<T0, T1> action)
+        {
+            @this.AddListener(action);
+            return Disposable.Create(() => { @this.RemoveListener(action); });
+        }
+
+        public static IDisposable Subscribe<T0, T1, T2>(
+            this UnityEvent<T0, T1, T2> @this, 
+            UnityAction<T0, T1, T2> action)
+        {
+            @this.AddListener(action);
+            return Disposable.Create(() => { @this.RemoveListener(action); });
+        }
+
+        public static IDisposable Subscribe<T0, T1, T2, T3>(
+            this UnityEvent<T0, T1, T2, T3> @this,
+            UnityAction<T0, T1, T2, T3> action)
+        {
+            @this.AddListener(action);
+            return Disposable.Create(() => { @this.RemoveListener(action); });
+        }
+    }
+}
