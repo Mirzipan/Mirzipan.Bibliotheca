72a9ffe8f1a28155a575536bb5e1bd7728674edc
Mirzipan <mfarkas@pixelfederation.com>
2023-02-19 17:11:15 +0100

    ✨added a container for CompositeDisposable and an extension method for an easier use

---
+++ Runtime/Disposables/DisposableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mirzipan.Bibliotheca.Disposables
+{
+    public static class DisposableExtensions
+    {
+        /// <summary>
+        /// Adds this <see cref="IDisposable"/> to the disposer.
+        /// </summary>
+        /// <param name="this"></param>
+        /// <param name="container">Object with disposer</param>
+        /// <returns></returns>
+        public static IDisposable DisposeWith(this IDisposable @this, IHaveDisposer container)
+        {
+            container.Disposer.Add(@this);
+            return @this;
+        }
+    }
+}
---
+++ Runtime/Disposables/IHaveDisposer.cs
@@ -0,0 +1,13 @@
+﻿namespace Mirzipan.Bibliotheca.Disposables
+{
+    /// <summary>
+    /// A container with composite disposable.
+    /// </summary>
+    public interface IHaveDisposer
+    {
+        /// <summary>
+        /// Gets or sets the instance of composite disposable.
+        /// </summary>
+        CompositeDisposable Disposer { get; set; }
+    }
+}
