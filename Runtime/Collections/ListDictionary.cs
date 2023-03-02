79178a3e52dc572c174f098c80a19fd78f88dcc4
Mirzipan <mfarkas@pixelfederation.com>
2023-03-02 12:04:33 +0100

    ✨added convenience dictionary collections (list, hashset and dictionary)

---
+++ Runtime/Collections/ListDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Pool;
+
+namespace Mirzipan.Bibliotheca.Collections
+{
+    public class ListDictionary<TKey, TValue>: CollectionDictionary<TKey, List<TValue>, TValue>
+    {
+        #region Protected
+
+        protected override List<TValue> CreateCollection()
+        {
+            return ListPool<TValue>.Get();
+        }
+
+        protected override List<TValue> CreateCollection(List<TValue> source)
+        {
+            var collection = ListPool<TValue>.Get();
+            collection.AddRange(source);
+            return collection;
+        }
+
+        protected override void DestroyCollection(List<TValue> collection)
+        {
+            ListPool<TValue>.Release(collection);
+        }
+
+        #endregion Protected
+    }
+}
