f6103464d23982f64bba420776e24d22b86c015e
Mirzipan <mfarkas@pixelfederation.com>
2023-03-16 20:37:24 +0100

    🐛fixed CompositeId drawer + made it prettier

--- Editor/CompositeIdDrawer.cs
+++ Editor/CompositeIdDrawer.cs
@@ -7,20 +7,42 @@ namespace Mirzipan.Bibliotheca.Editor
     [CustomPropertyDrawer(typeof(CompositeId))]
     public class CompositeIdDrawer : PropertyDrawer
     {
+        private const float SubLabelSpacing = 4;
+        private const float BottomSpacing = 2;
+        
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            SerializedProperty value = property.FindPropertyRelative("Value").FindPropertyRelative("Value");
-            var composite = new CompositeId(new OctoByte((ulong)value.longValue));
+            position.height -= BottomSpacing;
+            label = EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            SerializedProperty value = property.FindPropertyRelative("Value");
+            var composite = new CompositeId((ulong)value.longValue);
             var q1 = composite.Primary;
             var q2 = composite.Secondary;
+            
+            // backup gui settings
+            int indent = EditorGUI.indentLevel;
+            float labelWidth = EditorGUIUtility.labelWidth;
+            
+            float width = (position.width - SubLabelSpacing) / 2;
+            position = new Rect(position.x, position.y, width, position.height);
 
-            var rect = EditorGUI.PrefixLabel(position, label);
-            float halfW = rect.width * .5f;
-            rect.width = halfW;
-            q1 = new QuadByte(EditorGUI.TextField(rect, q1.ToString()));
-            rect.x += halfW;
-            q2 = new QuadByte(EditorGUI.TextField(rect, q2.ToString()));
-            value.longValue = (long)(ulong)new CompositeId(q1, q2);
+            var q1Content = new GUIContent("P");
+            var q2Content = new GUIContent("S");
+            
+            EditorGUI.indentLevel = 0;
+            EditorGUIUtility.labelWidth = EditorStyles.label.CalcSize(q1Content).x;
+            q1 = new QuadByte(EditorGUI.TextField(position, q1Content, q1.ToString()));
+            position.x += width + SubLabelSpacing;
+            q2 = new QuadByte(EditorGUI.TextField(position, q2Content, q2.ToString()));
+            value.longValue = (long)new CompositeId(q1, q2).Value;
+            
+            // restore gui settings
+            EditorGUIUtility.labelWidth = labelWidth;
+            EditorGUI.indentLevel = indent;
+            
+            EditorGUI.EndProperty();
         }
     }
 }
